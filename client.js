3c39e6636d063255f7ba214daaf943665efabbe1
William <Williamtviet@hotmail.com>
2023-02-21 17:16:39 -0800

    added name to snek

--- client.js
+++ client.js
@@ -12,6 +12,11 @@ const connect = function () {
   console.log (data)
 })
 
+conn.on ("connect", () => {
+  console.log ("Successfully connected to the game server")
+  conn.write ("Name: WTV")
+})
+
   return conn;
 };
 
